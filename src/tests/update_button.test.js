2a84167c8345d6e79c5170361283c8488b1103b3
Ville <visavola@gmail.com>
2020-08-19 00:22:45 +0300

    Testing

---
+++ src/tests/update_button.test.js
@@ -0,0 +1,9 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import Update from '../update_button';
+
+
+
+it('expect to render', () => {
+    expect(shallow(<div></div>)).toEqual(1)
+})
