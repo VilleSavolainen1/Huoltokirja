e5abc2be5573543b0abc76d9c75f901fe8e6b045
Ville <visavola@gmail.com>
2020-08-26 16:38:07 +0300

    Code formatted

--- src/job_update.js
+++ src/job_update.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const Jobupdate = ({ jobsToRender, id, modify, setModify, setJobIsUpdated }) => {
-
+const Jobupdate = ({
+  jobsToRender,
+  id,
+  modify,
+  setModify,
+  setJobIsUpdated,
+}) => {
   const getDate = () => {
     let day = JSON.stringify(new Date()).slice(1, 11).split("-").reverse();
-    return (`${day[0]}.${day[1]}.${day[2]}`)
-
-  }
+    return `${day[0]}.${day[1]}.${day[2]}`;
+  };
 
   const [day, setDay] = useState(getDate());
   const [info, setInfo] = useState(jobsToRender.lisätietoja);
 
-  
   const handlePvm = (e) => {
-    console.log(e.target.value)
+    console.log(e.target.value);
     setDay(e.target.value);
   };
 
@@ -24,14 +27,11 @@ const Jobupdate = ({ jobsToRender, id, modify, setModify, setJobIsUpdated }) => 
 
   const handleSubmit = (e) => {
     const changed = { pvm: day, lisätietoja: info };
-    axios
-      .put(`/api/jobs/${id}`, changed)
+    axios.put(`/api/jobs/${id}`, changed);
     e.preventDefault();
     setModify(false);
-    setJobIsUpdated(true)
+    setJobIsUpdated(true);
   };
-
-
 
   return (
     <div>
